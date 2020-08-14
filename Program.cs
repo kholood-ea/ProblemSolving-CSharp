8153893e72a17e033507a81df38908d9354567da
Kholood <kholood@Kholoods-MacBook-Pro.local>
2020-08-14 10:32:25 +0200

    newBranch

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace cciProblemSolving
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
