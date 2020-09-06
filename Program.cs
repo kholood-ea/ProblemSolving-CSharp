0f12cab659fae16b38f73a3e1c56584ae5fcadce
Kholood <kholood@Kholoods-MacBook-Pro.local>
2020-09-06 18:02:48 +0200

    Remove outer parentheses

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace leetCode
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            System.Console.WriteLine(RemoveOuterParentheses.removeOuterParentheses("(()())(())"));
         }
     }
 }
---
+++ UnitTesting/testCaseone.cs
@@ -0,0 +1,26 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace cciProblemSolving
+{
+
+
+    [TestClass]
+
+    public class intersectionLLUnitTest
+    {
+        [TestMethod]
+        public void intersectionShouldReturnNode()
+
+        {
+
+
+        }
+        [TestMethod]
+        public void intersectionShouldReturnNull()
+
+        {
+
+        }
+    }
+
+}
