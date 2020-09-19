c9933d8ce5d7f2042ba611c97a8a594413594d64
Kholood <kholood@Kholoods-MacBook-Pro.local>
2020-09-19 09:00:04 +0200

    maximum element

--- Waiter.cs
+++ Waiter.cs
@@ -6,62 +6,9 @@ namespace HackerRank
 {
     public class Waiter
     {
-
         public static int[] waiter(int[] number, int q)
         {
             List<int> result = new List<int>();
-            Stack<int> A = new Stack<int>();
-            Stack<int> B = new Stack<int>();
-            Stack<int> B1 = new Stack<int>();
-
-            List<int> PrimeNumbers = new List<int>();
-            PrimeNumbers.Add(2);
-            for (int i = 3; i <= 1200; i++)
-            {
-                if (i % 2 != 0)
-                {
-                    PrimeNumbers.Add(i);
-                }
-            }
-            for (int i = 0; i < q - 1; i++)
-            {
-                for (int j = number.Length - 1; j >= 0; j--)
-                {
-                    if (number[j] % PrimeNumbers[i] != 0)
-                    {
-                        A.Push(number[j]);
-                    }
-                    else
-                    {
-                        B.Push(number[j]);
-                    }
-                }
-                while (i != q - 1)
-                {
-                    B1 = A;
-                    A.Clear();
-                    if (B1.Peek() % PrimeNumbers[i + 1] == 0)
-                    {
-                        A.Push(B1.Pop());
-                    }
-
-
-
-                }
-            }
-            while (B.Count != 0)
-            {
-                System.Console.WriteLine($"{B.Peek()} Stack B");
-                result.Add(B.Peek());
-                B.Pop();
-            }
-            while (A.Count != 0)
-            {
-                System.Console.WriteLine($"{A.Peek()} Stack A");
-
-                result.Add(A.Peek());
-                A.Pop();
-            }
 
             return result.ToArray();
 
