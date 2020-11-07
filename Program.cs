dd26a0605713efe7d41ecb13fcd908d409260bcf
Kholood Ea <kholood@MacBooks-MacBook-Pro.local>
2020-11-07 14:08:32 +0200

    package updates

--- Program.cs
+++ Program.cs
@@ -6,7 +6,11 @@ namespace cciProblemSolving
     {
         static void Main(string[] args)
         {
-            StackWithMin2 obj = new StackWithMin2();
+
+int[,] arr=new int[2,3];
+int[,] arr1 = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+System.Console.WriteLine(arr.GetLength(2));
+            // StackWithMin2 obj = new StackWithMin2();
             // StackWithMin obj = new StackWithMin();
             // FixedMultiStack obj = new FixedMultiStack(5);
             // obj.push(0, 1);
@@ -14,15 +18,15 @@ namespace cciProblemSolving
             // obj.push(0, 3);
             // obj.push(0, 4);
             // obj.push(0, 5);
-            obj.push(1);
-            obj.push(2);
-            obj.push(3);
-            obj.push(4);
-            obj.push(5);
-            foreach (var item in obj.s2)
-            {
-                Console.WriteLine(item);
-            }
+            // obj.push(1);
+            // obj.push(2);
+            // obj.push(3);
+            // obj.push(4);
+            // obj.push(5);
+            // foreach (var item in obj.s2)
+            // {
+            //     Console.WriteLine(item);
+            // }
             // LinkedListNode sec = new LinkedListNode(2);
             // LinkedListNode first = new LinkedListNode(1, sec, null);
             // LinkedListNode third = new LinkedListNode(3, null, null);
